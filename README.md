57c00df90f4da9645d477068474b94bbbdffb9a7
Robert Brunson <robert.brunson@ext.superleague.com>
2023-06-04 21:08:57 -0400

    add readme and vscode keybindings

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotfiles
+This repository is mainly just to keep track of the dotfiles I use on my arch linux machine.
+This list is not by any means exhaustive and will change wildy over time :]
+
+## Configuration List
+- alacritty
+- zsh
--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
-# setup alacritty
-
+# path of the script
 CUR_PATH=$(dirname $(readlink -f "$0"))
-echo "Configuring Alacritty"
+
+# alacritty (https://github.com/alacritty/alacritty)
+echo "Configuring alacritty"
+echo "> installing alacritty.yml"
 ln -s -f $CUR_PATH/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
+# zsh (https://www.zsh.org/)
 echo "Configuring zsh"
+echo "> installing .zshrc"
 ln -s -f $CUR_PATH/zsh/.zshrc ~/.zshrc
+
+# vscode (https://code.visualstudio.com/)
+echo "configuring vscode"
+echo "> installing keybindings.json"
+ln -s -f $CUR_PATH/code/keybindings.json ~/.config/Code/User/keybindings.json
